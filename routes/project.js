3eefdac2cb16488025f5fcb08ddbc9af10afe5d2
Jeremy Leu <j3leu@ucsd.edu>
2015-02-19 22:17:20 +0000

    done

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,9 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project
+    .find({ "_id" : projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -16,13 +19,31 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  /*var newProject = new models.Project({
+    "title": form_data.project_title,
+    "date": form_data.date,
+    "summary": form_data.summary,
+    "image": form_data.image_url});*/
+  var newProject = new models.Project(form_data);
+  newProject.save(afterSave);
+  function afterSave(err, projects) {
+    if(err) console.log(err);
+    res.send();
+  }
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
-
+  models.Project
+    .find({ "_id" : projectID})
+    .remove()
+    .exec(afterQuery);
+    function afterQuery(err, projects) {
+      if(err) console.log(err);
+      res.send();
+    }
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
